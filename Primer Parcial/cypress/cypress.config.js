b049fa1f619f2ad9ac31fd59c69f6669ba785aaa
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2024-12-02 16:15:36 -0500

    Add timeout

--- Primer Parcial/cypress/cypress.config.js
+++ Primer Parcial/cypress/cypress.config.js
@@ -6,7 +6,8 @@ module.exports = defineConfig({
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.js",
   },
-
+  defaultCommandTimeout: 30000,
+  pageLoadTimeout: 60000,
   component: {
     devServer: {
       framework: "create-react-app",
--- Primer Parcial/cypress/cypress/e2e/contact/contact.cy.js
+++ Primer Parcial/cypress/cypress/e2e/contact/contact.cy.js
@@ -1,25 +1,20 @@
 it('debería permitir escribir en los campos del formulario', () => {
-    cy.visit('http://localhost:3000/contact');
+    cy.visit('/contact');
   
-    // Escribir en el campo Nombre
     cy.get('input#formName').type('Juan Pérez').should('have.value', 'Juan Pérez');
   
-    // Escribir en el campo Email
     cy.get('input#formEmail').type('juan.perez@example.com').should('have.value', 'juan.perez@example.com');
   
-    // Escribir en el campo Mensaje
     cy.get('textarea#formMessage').type('Este es un mensaje de prueba.').should('have.value', 'Este es un mensaje de prueba.');
   });
   
 
   it('debería validar que el campo Email contenga un "@" antes de enviar', () => {
-    cy.visit('http://localhost:3000/contact');
+    cy.visit('/contact');
     
-    // Intentar escribir un email sin "@" y enviar
     cy.get('input#formEmail').type('juan.perezexample.com');
     cy.get('button[type="submit"]').click();
     
-    // Verificar que el formulario no se envió (puedes validar mensajes o estados)
     cy.get('form').then(($form) => {
       expect($form[0].checkValidity()).to.be.false; // Validación HTML5
     });
